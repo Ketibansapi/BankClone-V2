68bb96d2e2a3d87a56d62c4f2f876d8d9fac93be
Deary <deary2706@gmail.com>
2018-11-25 16:59:56 +0700

    Handled auth

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI:'mongodb://Deary:Batamania12@ds119350.mlab.com:19350/clone-bank',
+  googleClientID:'224405101489-6qr2gg3v3i846tp6d0missvl6jig4oq2.apps.googleusercontent.com',
+  googleClientSecret:'swIIM5JXg5CkmPTOYk3l6u8I'
+}
